7376dbeff991df44611516702ff91a6768affd6c
sgilliland <sarah13grace@gmail.com>
2020-06-28 22:47:22 -0500

    Restarts after contact with infected person

--- Assets/GameManagerS1.cs
+++ Assets/GameManagerS1.cs
@@ -1,10 +1,16 @@
 ﻿
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManagerS1 : MonoBehaviour
 {
     public void EndGame()
 	{
-		
+		Restart();
+	}
+	
+	void Restart()
+	{
+		SceneManager.LoadScene(SceneManager.GetActiveScene().name);
 	}
 }
